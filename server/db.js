658e6857da97c96dd316191b6e2648e57af34d81
avithew <avithew@gmail.com>
2024-12-01 02:19:23 -0700

    final touches

--- server/db.js
+++ server/db.js
@@ -28,13 +28,78 @@ const createTables = async () => {
   client.query(SQL);
 };
 
+const createCustomer = async (name) => {
+  const SQL = `
+        INSERT INTO customers(id, name) VALUES($1,$2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+const createRestaurant = async (name) => {
+  const SQL = `
+        INSERT INTO restaurants(id, name) VALUES($1,$2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+const createReservation = async (
+  date,
+  party_count,
+  restaurant_id,
+  customer_id
+) => {
+  const SQL = `
+        INSERT INTO reservations(id, date, party_count, restaurant_id, customer_id) 
+        VALUES($1,$2,$3,$4,$5) RETURNING *
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    date,
+    party_count,
+    restaurant_id,
+    customer_id,
+  ]);
+  return response.rows[0];
+};
+
+const fetchCustomers = async () => {
+  const SQL = `
+        SELECT * FROM customers
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+const fetchRestaurants = async () => {
+  const SQL = `
+        SELECT * FROM restaurants
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+const fetchReservations = async () => {
+  const SQL = `
+          SELECT * FROM reservations
+      `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const destroyReservation = async (id, customer_id) => {
+  const SQL = `
+        DELETE FROM reservations
+        WHERE id=$1 AND customer_id=$2
+    `;
+  await client.query(SQL, [id, customer_id]);
+};
+
 module.exports = {
   client,
   createTables,
-  //   createCustomer,
-  //   createRestaurant,
-  //   fetchCustomers,
-  //   fetchRestaurants,
-  //   createReservation,
-  //   destroyReservation,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
+  fetchReservations,
+  createReservation,
+  destroyReservation,
 };
--- server/index.js
+++ server/index.js
@@ -1,32 +1,37 @@
 const {
   client,
   createTables,
-  //   createCustomer,
-  //   createRestaurant,
-  //   fetchCustomers,
-  //   fetchRestaurants,
-  //   createReservation,
-  //   destroyReservation,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
+  fetchReservations,
+  createReservation,
+  destroyReservation,
 } = require("./db");
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/api/customers", async (req, res, next) => {
   try {
+    res.send(await fetchCustomers());
   } catch (ex) {
     next(ex);
   }
 });
 app.get("/api/restaurants", async (req, res, next) => {
   try {
+    res.send(await fetchRestaurants());
   } catch (ex) {
     next(ex);
   }
 });
 app.get("/api/reservations", async (req, res, next) => {
   try {
+    res.send(await fetchReservations());
   } catch (ex) {
     next(ex);
   }
@@ -34,6 +39,16 @@ app.get("/api/reservations", async (req, res, next) => {
 
 app.post("/api/customers/:id/reservations", async (req, res, next) => {
   try {
+    res
+      .status(201)
+      .send(
+        await createReservation(
+          req.body.date,
+          req.body.party_count,
+          req.body.restaurant_id,
+          req.params.id
+        )
+      );
   } catch (ex) {
     next(ex);
   }
@@ -43,6 +58,8 @@ app.delete(
   "/api/customers/:customer_id/reservations/:id",
   async (req, res, next) => {
     try {
+      await destroyReservation(req.params.id, req.params.customer_id);
+      res.sendStatus(204);
     } catch (ex) {
       next(ex);
     }
@@ -53,6 +70,24 @@ const init = async () => {
   client.connect();
   console.log("connected to database");
   createTables();
+  const [leo, alex, tyler, sky] = await Promise.all([
+    createCustomer("Leo"),
+    createCustomer("Alex"),
+    createCustomer("Tyler"),
+    createCustomer("Sky"),
+  ]);
+  const [sushi_haven, bella_cucina, ocean_bistro] = await Promise.all([
+    createRestaurant("Sushi Haven"),
+    createRestaurant("La Bella Cucina"),
+    createRestaurant("Blue Ocean Bistro"),
+  ]);
+  const [] = await Promise.all([
+    createReservation("02/03/2021", 4, sushi_haven.id, sky.id),
+    createReservation("02/15/2021", 3, bella_cucina.id, alex.id),
+  ]);
+  app.listen(port, () => {
+    console.log("listening to port: ", port);
+  });
 };
 
 init();
